45dc93acb44342eba889328c18fb3441b49fd160
Sean Ryan <sean@tech1337.co.uk>
2017-07-14 17:15:45 +0100

    Created CRUD routes and tested the MongoDB connectivity with a simple get request.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+
+var url = 'mongodb://tech1337:asshole@ds159112.mlab.com:59112/ogapp';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function(req, res, next){
+    res.render('index');
+});
+
+router.get('/load', function(req, res, next) {
+    var data = [];
+    mongo.connect(url, function(err, db){
+        if (!err){
+            var item = db.collection('documents').find();
+            item.forEach(function(record, err){
+                if(!err){
+                    data.push(record);
+                }
+            }, function () {
+                res.render('index', {results: data});
+                db.close();
+                console.log(data);
+            });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/save', function(req, res, next){
+
+});
+
+router.get('/new', function(req, res, next){
+
+});
+
+router.get('/delete', function(req, res, next){
+
 });
 
 module.exports = router;
