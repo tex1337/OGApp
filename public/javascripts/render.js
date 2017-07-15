4f14d95ffdc6b3c1ff121ce1383095009536cc0d
Sean Ryan <sean@tech1337.co.uk>
2017-07-15 08:18:52 +0100

    Refactored the circles generation to a function for modularity, ease of reading and generated some styles for the canvas DOM element to get around the cludge of using the inputArea box instead.

--- public/javascripts/render.js
+++ public/javascripts/render.js
@@ -1,8 +1,11 @@
 /**
  * Created by Sean Ryan on 7/14/2017.
  * Using KonvaJS: Don't reinvent the wheel.
+ *
+ * P.S: This is my first real use of HTML5 Canvas!
+ *      Lucky I know some P5.js / Processing!
+ *
  */
-
 
 // Create a 'stage' as canvas
 var stage = new Konva.Stage({
@@ -17,9 +20,7 @@ var layer = new Konva.Layer();
 var inputArea = new Konva.Rect({
     width: 500,
     height: 500,
-    fill: 'white',
-    stroke: 'black',
-    strokeWidth: 2
+    fill: 'white'
 });
 
 layer.add(inputArea).draw();
@@ -39,37 +40,17 @@ var Circles = new Konva.Group();
 //     draggable: true
 // });
 
-// Push test to array
+// Push test to image group
 //Circles.add(testCircle);
 
 // Detect mouseevent click on inputArea
 inputArea.on('click', function(e) {
-
     // If this triggered, we weren't in a circle
-    // so bash a new one out.
-    var newCirc = new Konva.Circle({
-        x: e.evt.clientX,
-        y: e.evt.clientY,
-        radius: 50,
-        fill: 'yellow',
-        stroke: 'black',
-        strokeWidth: 4,
-        draggable: true
-    });
+    // so bash a new one out, only on left-mouse.
+    if(e.evt.button === 0){
+        circleFactory(e.evt.clientX, e.evt.clientY);
+    }
 
-    newCirc.on('mouseover dragstart', function() {
-        this.moveToTop();
-        layer.draw();
-    });
-
-    newCirc.on('dblclick dbltap', function(e) {
-        this.destroy();
-        layer.draw();
-    });
-
-    // There could be some
-    Circles.add(newCirc);
-    doUpdate();
 });
 
 // Run view update
@@ -84,3 +65,34 @@ function doUpdate(){
     layer.add(Circles);
     stage.add(layer);
 }
+
+function circleFactory(X, Y){
+
+    var newCirc = new Konva.Circle({
+        x: X,
+        y: Y,
+        radius: 50,
+        fill: 'yellow',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable: true
+    });
+
+    newCirc.on('mouseover dragstart', function() {
+        this.moveToTop();
+        layer.draw();
+    });
+
+    newCirc.on('dblclick dbltap', function() {
+        this.destroy();
+        layer.draw();
+    });
+
+    // Push new circle and update the view
+    Circles.add(newCirc);
+    doUpdate();
+}
+
+// <canvas> is dynamically gen, need this to add CSS to the DOM element.
+var canv = document.querySelector('canvas');
+canv.style.border = "3px solid black";
